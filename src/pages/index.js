0450e27b9930b43bcfd55d40ca694580663a473f
micleners <graindesk@Sources-MacBook-Pro.local>
2020-03-04 21:47:51 -0600

    Add icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,23 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+import {
+  FaFacebookSquare,
+  FaLinkedin,
+  FaTwitter,
+  FaPinterest,
+  FaInstagram,
+} from "react-icons/fa"
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
+    <FaFacebookSquare />
+    <FaLinkedin />
+    <FaTwitter />
+    <FaPinterest />
+    <FaInstagram />
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
