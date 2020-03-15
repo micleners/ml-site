6bf31d393c2c177c1aaf84791c8bb3742d045b6e
Michael Leners <micleners@gmail.com>
2020-03-14 23:49:51 -0500

    Add bio to home

--- src/components/homeTemplate/homeIntroPanel.js
+++ src/components/homeTemplate/homeIntroPanel.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
-import { Flex, Text, Box } from "rebass"
+import { Flex, Text, Box , Heading} from "rebass"
 
 const AboutMeList = props => (
   <Text
@@ -29,21 +29,49 @@ export const HomeIntroPanel = () => {
   `)
 
   return (
-    <Flex py="100px" justifyContent="space-between" alignItems="flex-end">
-      <Box mb="2" width="275px">
-        <Img
-          style={{ width: "100%" }}
-          fluid={data.micleners.childImageSharp.fluid}
-        />
-      </Box>
-      <Text width="200px" minWidth="140px" fontSize="3" mx={[3, 4]} lineHeight="1.35">
-        I’m Michael Leners
-      </Text>
-      <Flex flexDirection="column" justifyContent="flex-end">
-        <AboutMeList mb="2">full-stack web developer</AboutMeList>
-        <AboutMeList mb="2">speaker</AboutMeList>
-        <AboutMeList>consultant</AboutMeList>
+    <>
+      <Flex pt="100px" justifyContent="space-between" alignItems="flex-end">
+        <Box mb="2" width="275px">
+          <Img
+            style={{ width: "100%" }}
+            fluid={data.micleners.childImageSharp.fluid}
+          />
+        </Box>
+        <Text
+          width="200px"
+          minWidth="140px"
+          fontSize="3"
+          mx={[3, 4]}
+          lineHeight="1.35"
+        >
+          I’m Michael Leners
+        </Text>
+        <Flex flexDirection="column" justifyContent="flex-end">
+          <AboutMeList mb="2">full-stack web developer</AboutMeList>
+          <AboutMeList mb="2">speaker</AboutMeList>
+          <AboutMeList>consultant</AboutMeList>
+        </Flex>
       </Flex>
-    </Flex>
+      <Heading pt={5} as="h1" fontSize={7}>
+        About Me
+      </Heading>
+      <Text pb={3}>
+        Formerly a physics University Instructor and Chemical Engineering
+        graduate student, I am now a web developer. While I originally was
+        attracted to data analysis, Web Development has captured my heart. It
+        keeps me engaged with the growth and learning potential it offers. I am
+        originally from outside of Seattle, WA and moved to Iowa for graduate
+        school in 2013. It’s probably time for me to get an “Iowa Captive”
+        bumper sticker.
+      </Text>
+      <Text pb={5}>
+        I thought I’d love backend because of my love for data analysis, but I
+        find myself drawn more towards frontend development. I have worked
+        professionally with Ember, Angular, Django, Dotnet Core, AWS and Azure.
+        On my own, I enjoy experimenting with Vue and Gatsby and “backend as a
+        service” options. I am a strong advocate for accessibility and diversity
+        in tech. I continually strive to become a better ally in these areas.
+      </Text>
+    </>
   )
 }
